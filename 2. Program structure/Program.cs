558b5693b532c2e12102b50e3874d93089142318
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-07 15:35:02 +0200

    Console

---
+++ 2. Program structure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2._Program_structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Кукусики");
+            Console.WriteLine("First block");
+            {
+                Console.WriteLine("Last block");
+            }
+            
+        }
+    }
+}
