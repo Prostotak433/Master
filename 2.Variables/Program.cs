dfa4eb7758cb9826ad2ecb3c5f82f92682b5e236
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-07 15:47:43 +0200

    2. Variables

---
+++ 2.Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2.Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = "Tom";
+
+            string Name = "lexa";
+            Console.WriteLine(Name);
+            Name = "Jmux";
+            Console.WriteLine(Name);
+            Console.ReadKey();
+        }
+    }
+}
