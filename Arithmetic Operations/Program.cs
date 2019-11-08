9cc04cae065e1fb8c5f97e53a59c4eb3b1f4b62e
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-08 14:38:34 +0200

    2. Arithmetic Operations

---
+++ Arithmetic Operations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Arithmetic_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 5;
+            int z1 = ++x1; 
+            Console.WriteLine($"{x1} - {z1}");
+
+            int x2 = 5;
+            int z2 = x2++;
+            Console.WriteLine($"{x2} - {z2}");
+
+            int x11 = 5;
+            int z11 = --x1; // z1=4; x1=4
+            Console.WriteLine($"{x11} - {z11}");
+
+            int x22 = 5;
+            int z22 = x2--; // z2=5; x2=4
+            Console.WriteLine($"{x22} - {z22}");
+
+        }
+    }
+}
