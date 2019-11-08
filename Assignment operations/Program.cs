54f4aa58a44b52ccde6e1010ca5b7bad212e57dd
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-08 15:01:49 +0200

    2.  Assignment operations

---
+++ Assignment operations/Program.cs
@@ -0,0 +1,22 @@
+﻿-using System;
+
+namespace Assignment_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            a += 10;        // 20
+            a -= 4;         // 16
+            a *= 2;         // 32
+            a /= 8;         // 4
+            a <<= 4;      // 64
+            a >>= 2;      // 16
+
+            int a1 = 8;
+            int b = 6;
+            int c = a1 += b -= 5;    // 9
+        }
+    }
+}
