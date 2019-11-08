92a607dc1f35df2d304107759ba3a31aca7ddb49
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-08 15:12:20 +0200

    2. Basic data type conversions

---
+++ Basic data type conversions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Basic_data_type_conversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 4;
+            byte b = (byte)(a + 70);
+
+            byte a1 = 4;             // 0000100
+            ushort b1= a1;   // 000000000000100
+
+            sbyte a2 = -4;           // 1111100
+            short b2= a2;    // 111111111111100
+
+            try
+            {
+                int a3 = 33;
+                int b3 = 600;
+                byte c = checked((byte)(a3 + b3));
+                Console.WriteLine(c);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
