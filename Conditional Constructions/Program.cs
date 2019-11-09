708f363b0c50a4722517a4c8fedc4e94e17a4bb2
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-09 15:22:49 +0200

    2. Conditional Constructions

---
+++ Conditional Constructions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Conditional_Constructions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 8;
+            int num2 = 10;
+            if(num1 < num2)
+            {
+                Console.WriteLine(num1 < num2);
+            }
+            else if (num1 > num2)
+            {
+                Console.WriteLine(num1 > num2);
+            }
+            else
+            {
+                Console.WriteLine(num1 = num2);
+            }
+            Console.WriteLine("нажмите Y или U");
+            string vubor = Console.ReadLine();
+            switch(vubor)
+            {
+                case "Y":
+                    Console.WriteLine("Y");
+                    break;
+                case "U":
+                    Console.WriteLine("U");
+                    break;
+                default:
+                    Console.WriteLine("bezdar6");
+                    break;
+            }
+            int x = 3;
+            int y = 4;
+            Console.WriteLine("нажмите + или -");
+            string result = Console.ReadLine();
+            int z = result == "+" ? (x + y) : (x - y);
+            Console.WriteLine(z);
+
+        }
+    }
+}
