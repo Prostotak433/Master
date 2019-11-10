abb588021846365690d275492f48a9a8a944103b
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-10 15:11:48 +0200

    2.Cycle

---
+++ Cycles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 9; i++)
+            {
+                if (i == 1)
+                    continue;
+                if (i == 3)
+                    break;
+                Console.WriteLine($"Квадрат числа {i} равен {i * i}");
+            }
+            var c = 9;
+            do
+            {
+                Console.WriteLine(c);
+                c--;
+            }
+            while (c > 0);
+        }
+    }
+}
