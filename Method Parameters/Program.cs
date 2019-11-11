8fb3fc5ff06f3713b22708a16852b25473c4641e
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-11 18:12:08 +0200

    2. Method Parameters

---
+++ Method Parameters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Method_Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int a = 25;
+            int b = 35;
+            int result = Sum(a, b);
+            Console.WriteLine(result);
+
+            result = Sum(b, 45);
+            Console.WriteLine(result);
+
+            result = Sum(a + b + 12, 18);
+            Console.WriteLine(result);
+
+            result = Sum(10, 15);
+            Console.WriteLine(result);
+        }
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        static void Display(string name, int age)
+        {
+            Console.WriteLine($"Name: {name}  Age: {age}");
+        }
+    }
+}
