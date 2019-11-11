c713de7b526ebb5c8dd1c38980dba60ed0745fc4
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-11 17:46:29 +0200

    2.Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opa = GetHellow();
+            Console.WriteLine(opa);
+            int hi = GetSum();
+            Console.WriteLine(hi);
+
+            SayGoodBye();
+            SayHello();
+        }
+        static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void SayGoodBye()
+        {
+            Console.WriteLine("Bye");
+        }
+        static int GetSum()
+        {
+            int x = 2;
+            int y = 3;
+            int z = x + y;
+            return z;
+        }
+        static string GetHello()
+        {
+            return "Hello";
+        }
+        static void Serega() => Console.WriteLine("serega ");
+        static string GetHellow() => "111111111111111111111111";
+    }
+}
