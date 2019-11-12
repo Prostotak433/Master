9c856aa1bbe58f5fcd988e3f1e77cd43989262de
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-12 23:26:08 +0200

    Referencse type  Value type

---
+++ Referencse type  Value type/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Referencse_type__Value_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 33;
+            Add(ref a, b);
+            Console.WriteLine(a);
+
+            int x = 10;
+            GetData(x, 15, out int area, out int perimetr);
+            Console.WriteLine("Площадь : " + area);
+            Console.WriteLine("Периметр : " + perimetr);
+
+        }
+        
+        
+        static void Add(ref int a,  int b)
+        {
+            a += b;
+        }
+        static void GetData(int x, int y, out int area, out int perim)
+        {
+            area = x * y;
+            perim = (x + y) * 2;
+        }
+    }
+}
