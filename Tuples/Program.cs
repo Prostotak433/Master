92426d4f63e0d4a89e04a864d6ef43ea975e40ce
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-13 16:50:49 +0200

    2.Tuples

---
+++ Tuples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = (19, count:30);
+            Console.WriteLine(type.Item1);
+            type.Item1 += 11;
+            Console.WriteLine(type.Item1);
+            Console.WriteLine(type.count);
+        }
+    }
+}
