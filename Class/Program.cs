23ef1a7b7cdb6900e478c0598da1e9ad83a71e4b
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-13 17:12:19 +0200

    3.Class

---
+++ Class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Class
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public Person(string n, int a) { Name = n; Age = a}
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {n}  Возраст: {a}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Age = 19;
+            person.Name = "LLL";
+            Console.WriteLine(person.Name);
+                
+        }
+    }
+}
