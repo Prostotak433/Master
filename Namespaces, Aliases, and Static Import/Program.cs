039154bcf2f2899ac1bea4de64c3b67210f32d05
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-14 16:57:31 +0200

    3. Namespaces, Aliases, and Static Import

---
+++ Namespaces, Aliases, and Static Import/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using printer = System.Console;
+
+
+namespace Namespaces__Aliases__and_Static_Import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printer.WriteLine("Hello from C#");
+            printer.Read();
+        }
+    }
+}
