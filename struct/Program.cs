712a6c9bc203bad4cadce0a0ffe2df753f31b0f0
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-14 15:13:36 +0200

    2. Struct

---
+++ struct/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User tom;
+            tom.Age = 12;
+            tom.Name = " TOM ";
+
+        }
+        struct User
+        {
+            public string Name;
+            public int Age;
+            public void Display()
+            {
+                Console.WriteLine($"Name: {Name}  Age: {Age}");
+            }
+        }
+    }
+}
