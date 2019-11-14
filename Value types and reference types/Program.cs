75dc61b5eda69903b60c4735b4c045868a609b87
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-14 16:43:58 +0200

    3.  Value types and reference types

---
+++ Value types and reference types/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Value_types_and_reference_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            State state1 = new State(); // State - структура, ее данные размещены в стеке
+            Country country1 = new Country(); // Country - класс, в стек помещается ссылка на адрес в хипе
+                                              // а в хипе располагаются все данные объекта country1
+        }
+    }
+    struct State
+    {
+        public int x;
+        public int y;
+        public Country country;
+    }
+    class Country
+    {
+        public int x;
+        public int y;
+    }
+    
+}
