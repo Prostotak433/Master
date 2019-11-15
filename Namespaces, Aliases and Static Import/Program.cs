6da8da22bb758dd3ee77f6cc58ecd5248864fe04
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-15 17:57:55 +0200

    3.Namespaces, Aliases, and Static Import

---
+++ Namespaces, Aliases and Static Import/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Namespaces__Aliases_and_Static_Import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
