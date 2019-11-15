5fdf499865f33e7dc7f2fa9f1950a0731a7f353c
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-15 23:24:23 +0200

    3. Class Library

---
+++ Class Library/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyLib;
+
+namespace Class_Library
+   
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tom = new Person { name = "Tom", age = 35 };
+            Console.WriteLine(tom.name);
+        }
+    }
+}
