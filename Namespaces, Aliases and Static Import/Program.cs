dda6ade1299e0937095736fd983aac4a095a7a83
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-15 23:16:40 +0200

    3.Namespaces, Aliases, and Static Import

--- Namespaces, Aliases and Static Import/Program.cs
+++ Namespaces, Aliases and Static Import/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-
+using Person = HelloApp.User;
+using Printer = System.Console;
 namespace Namespaces__Aliases_and_Static_Import
 {
-    class Program
+   HelloApp
     {
-        static void Main(string[] args)
+        class Program
         {
-            
+            static void Main(string[] args)
+            {
+                Person person = new Person();
+                person.name = "Tom";
+                Printer.WriteLine(person.name);
+                Printer.Read();
+            }
+        }
+
+        class User
+        {
+            public string name;
         }
     }
-}
