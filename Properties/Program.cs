847a3907ac6b3a3aa667cb816701c2537fc53663
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-16 15:23:06 +0200

    3.  Properties

---
+++ Properties/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }    
+    }
+    class Person
+    {
+        private int pluse = 15;
+        private string name;
+        public string Name
+        {
+            
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+        }
+
+}
