23f904dcc4ce562e0068b39ef49aa3d8df68058b
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-17 13:45:11 +0200

    3.  Read constants, fields, and structures

---
+++ Read constants, fields, and structures/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Read_constants__fields__and_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                  
+        }
+    }
+    readonly struct User
+    {
+        const double e = 2.71;
+    }
+}
+
