c9bf4967abc1c1bbc2a808d381828505010a1cb5
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-17 13:51:14 +0200

    3 .Null

---
+++ Null/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Null
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object s = null;
+            string o = null;
+            if (user != null && user.Phone != null && user.Phone.Company != null)
+            {
+                string companyName = user.Phone.Company.Name;
+                Console.WriteLine(companyName);
+            }
+        }
+    }
+}
