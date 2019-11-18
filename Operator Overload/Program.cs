d25aa58d272a1d9d3583672d1f1df9a4e9b718ca
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-18 18:28:30 +0200

    3. Operator Overload

--- Operator Overload/Program.cs
+++ Operator Overload/Program.cs
@@ -6,7 +6,34 @@ namespace Operator_Overload
     {
         static void Main(string[] args)
         {
-            
+            Counter c1 = new Counter { Value = 23 };
+            Counter c2 = new Counter { Value = 33 };
+            bool result = c2 > c1;
+            Console.WriteLine(result);
+            Counter c3 = c1 + c2;
+            Console.WriteLine(c3.Value);
+            int z = c1 + 23;
+            Console.WriteLine(z);
+        }
+    }
+    class Counter
+    {
+        public int Value { get; set; }
+        public static Counter operator +(Counter c1, Counter c2)
+        {
+            return new Counter { Value = c1.Value + c2.Value };
+        }
+        public static bool operator >(Counter c1, Counter c2)
+        {
+            return c1.Value > c2.Value;
+        }
+        public static bool operator <(Counter c1, Counter c2)
+        {
+            return c1.Value < c2.Value;
+        }
+        public static int operator +(Counter c1, int val)
+        {
+            return c1.Value + val;
         }
     }
 }
