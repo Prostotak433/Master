6b516786020c88914528f2a6d177409683af9ced
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-19 15:14:10 +0200

    3. Index

---
+++ Index/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People people = new People();
+            people[0] = new Person { Name = "" };
+            people[1] = new Person { Name = "Lexa" };
+            Person tom = people[0];
+            Console.WriteLine(tom?.Name);
+            Console.ReadKey();
+        }
+    }
+    class People
+    {
+        Person[] data;
+        public People()
+        {
+            data = new Person[5];
+        }
+        public Person this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+            set
+            {
+                data[index] = value;
+            }
+        }
+
+    }
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
