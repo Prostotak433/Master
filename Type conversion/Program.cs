cf3bd3bdf0aa1814f772b589ff4241bcb69d9311
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-20 16:32:22 +0200

    3. Type Conversion

--- Type conversion/Program.cs
+++ Type conversion/Program.cs
@@ -9,4 +9,34 @@ namespace Type_conversion
             
         }
     }
+    class Person
+    {
+        public string Name { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Person {Name}");
+        }
+    }
+
+    class Employee : Person
+    {
+        public string Company { get; set; }
+        public Employee(string name, string company) : base(name)
+        {
+            Company = company;
+        }
+    }
+
+    class Client : Person
+    {
+        public string Bank { get; set; }
+        public Client(string name, string bank) : base(name)
+        {
+            Bank = bank;
+        }
+    }
 }
