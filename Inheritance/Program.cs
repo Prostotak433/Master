43f869a9dc23aa4d9bbd70cdf324b4ca2fd65462
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-20 15:10:50 +0200

    3.  Inheritance

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,7 +6,37 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+            Person p = new Person("bob");
+            p.Display();
+            Employee emp = new Employee ("non","WTF" );
+            emp.Display();
             
+
         }
+    }
+    class Person
+    {
+        private string _name;
+        public string Name { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public void Display()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+    class Employee : Person
+    {
+        public string Company;
+        public Employee(string name, string company) : base(name)
+        {
+            Company = company;
+        }
+    }
+    sealed class Admin
+    {
+
     }
 }
