e4613c14481212fb44f1303486a34ed4b3991124
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-21 16:32:18 +0200

    3. Virtual Methods and Properties

---
+++ Virtual Methods and Properties/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Virtual_Methods_and_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public virtual void Display()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+    class Employee : Person
+    {
+        public string Company { get; set; }
+        public Employee(string name, string company) : base(name)
+        {
+            Company = company;
+        }
+        public override void Display()
+        {
+            Console.WriteLine($"{Name} работает в {Company}");
+        }
+    }
+}
