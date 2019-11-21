1858474680e93d79166137a49f60317dd6e3831b
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-21 15:25:02 +0200

    3. Overloading Type Conversion Operations

---
+++ Overloading Type Conversion Operations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Overloading_Type_Conversion_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter1 = new Counter { Seconds = 33 };
+            int x = (int)counter1;
+            Console.WriteLine(x);
+            Counter counter2 = x;
+            Console.WriteLine(counter2.Seconds);
+        }
+    }
+    class Counter
+    {
+        public int Seconds { get; set; }
+        public static implicit operator Counter(int x)
+        {
+            return new Counter { Seconds = x };
+        }
+        public static explicit operator int(Counter counter)
+        {
+            return counter.Seconds;
+        }
+    }
+}
