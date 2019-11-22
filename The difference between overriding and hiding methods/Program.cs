ced5b2674e91707d486a3727e3ea5e14cb395e95
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-22 17:16:00 +0200

    3.The difference between overriding and hiding methods

---
+++ The difference between overriding and hiding methods/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace The_difference_between_overriding_and_hiding_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tom = new Employee("Tom", "Microsoft");
+            tom.Display();      // Tom
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    class Employee : Person
+    {
+        public string Company { get; set; }
+        public Employee(string name, string company)
+                : base(name)
+        {
+            Company = company;
+        }
+        public new void Display()
+        {
+            Console.WriteLine($"{Name} работает в {Company}");
+        }
+    }
+}
