208c7704c37ea403ce264e5367f47e625649c09e
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-22 12:43:45 +0200

    3. Shadowing / hiding

---
+++ shadowing  hiding/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace shadowing__hiding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person bob = new Person("Bob", "Robertson");
+            bob.Display();      // Bob Robertson
+
+            Employee tom = new Employee("Tom", "Smith", "Microsoft");
+            tom.Display();      // Tom Smith работает в Microsoft
+
+            Console.ReadKey();
+        }
+    }
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+        }
+    }
+    class Employee : Person
+    {
+        public string Company { get; set; }
+        public Employee(string firstName, string lastName, string company) : base(firstName, lastName)
+        {
+            Company = company;
+        }
+        public new void Display()
+        {
+            Console.WriteLine($"{FirstName} {LastName} работает в {Company}");
+        }
+    }
+
+   
+
+}
