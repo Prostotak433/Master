98117b7a6fe4cfadca0275892871feddfed400cf
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-22 17:38:40 +0200

    3. Abstract Class

--- Abstract class/Program.cs
+++ Abstract class/Program.cs
@@ -6,7 +6,44 @@ namespace Abstract_class
     {
         static void Main(string[] args)
         {
-            
+            Client client = new Client("Tom", 500);
+            Employee employee = new Employee("Bob", "Apple");
+            client.Display();
+            employee.Display();
+        }
+    }
+    abstract class Person
+    {
+      public string Name { get; set; }
+      public Person(string name)
+        {
+            Name = name;
+        }
+        public void Display()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+    class Client : Person
+    {
+        public int Sum { get; set; }    // сумма на счету
+
+        public Client(string name, int sum)
+            : base(name)
+        {
+            Sum = sum;
+        }
+    }
+    class Employee : Person
+    {
+        public string Position { get; set; } // должность
+
+        public Employee(string name, string position)
+            : base(name)
+        {
+            Position = position;
         }
     }
 }
+
+
