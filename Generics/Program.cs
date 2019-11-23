40fae2ea92260cdd019408b616751075b2fe4162
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-23 16:36:50 +0200

    3. Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int x = 7;
+            int y = 25;
+            Swap<int>(ref x, ref y); // или так Swap(ref x, ref y);
+            Console.WriteLine($"x={x}    y={y}");   // x=25   y=7
+
+            string s1 = "hello";
+            string s2 = "bye";
+            Swap<string>(ref s1, ref s2); // или так Swap(ref s1, ref s2);
+            Console.WriteLine($"s1={s1}    s2={s2}"); // s1=bye   s2=hello
+
+            Console.Read();
+        }
+        public static void Swap<T>(ref T x, ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
