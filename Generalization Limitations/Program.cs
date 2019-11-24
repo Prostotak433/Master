45b16b81fe5f26386e08d291be75a2b710fc90d8
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-24 13:56:20 +0200

    3. Generalization Limitations

---
+++ Generalization Limitations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Generalization_Limitations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(3000) { Sum = 4500 };
+            Account account1 = new Account(1777) { Sum = 7000 };
+            Transaction<Account> transaction1 = new Transaction<Account> {FromAccount = account,ToAccount = account1,Sum = 3900};
+            transaction1.Exe();
+            Console.Read();
+        }
+    }
+    class Account
+    {
+        public int Id { get; private set; } // номер счета
+        public int Sum { get; set; }
+        public Account(int _id)
+        {
+            Id = _id;
+        }
+    }
+    class Transaction<T> where T : Account
+    {
+        public T FromAccount { get; set; }
+        public T ToAccount { get; set; }
+        public int Sum { get; set; }
+        public void Exe()
+        {
+            if (FromAccount.Sum > Sum)
+            {
+                FromAccount.Sum -= Sum;
+                ToAccount.Sum += Sum;
+                Console.WriteLine($"Счет {FromAccount.Id}: {FromAccount.Sum}$ \nСчет {ToAccount.Id}: {ToAccount.Sum}$");
+            }
+            else
+            {
+                Console.WriteLine($"Недостаточно денег на счете {FromAccount.Id}");
+            }
+        }
+    }
+}
