1888f48fd56895f7ec00d9fe73d77a2ccf8869fc
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-25 17:40:35 +0200

    5. String and Class System.String

---
+++ 5. String and Class System.String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5._String_and_Class_System.String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "hello";
+            char ch1 = s1[1];
+            Console.WriteLine(ch1);
+            Console.WriteLine(s1.Length);
+          
+        }
+        
+    }
+}
+   
+    
+
