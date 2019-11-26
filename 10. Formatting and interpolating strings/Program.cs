08ab6f822111a3973a66e97ccecb13e12afa69ee
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-26 21:07:27 +0200

    10. Formatting and interpolating strings

---
+++ 10. Formatting and interpolating strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10._Formatting_and_interpolating_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person { Age = 23, Name = "Tom" };
+            Console.WriteLine("name {0} age{1}", person.Name, person.Age);
+            long number = 380984122055;
+            string result = number.ToString("{0:+# (###) ### ## ##}");
+            Console.WriteLine(result);
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
