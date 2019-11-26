47b0cc1a48171f1ccb4fa21595c8d7f4e4304d20
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-26 21:56:31 +0200

    10. Regular expressions

---
+++ Regular expressions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace Regular_expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Бык тупогуб, тупогубенький бычок, у быка губа бела была тупа";
+            Regex regex = new Regex(@"туп(\w*)");
+            MatchCollection matches = regex.Matches(s);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    Console.WriteLine(match.Value);
+            }
+            else
+            {
+                Console.WriteLine("Совпадений не найдено");
+            }
+            string n = "456-435-2318";
+            Regex regex1 = new Regex(@"\d{3}-\d{3}-\d{4}");
+        }
+    }
+}
