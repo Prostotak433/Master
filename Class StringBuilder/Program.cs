d820d1ae4e664363997a518a653c05710b657ca4
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-26 21:33:05 +0200

    10. Class StringBuilder

---
+++ Class StringBuilder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+namespace Class_StringBuilder
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("Привет мир");
+            sb.Append("!");
+            sb.Insert(7, "компьютерный ");
+            Console.WriteLine(sb);
+            sb.Replace("мир", "world");
+            Console.WriteLine(sb);
+            sb.Remove(7, 13);
+            Console.WriteLine(sb);
+            string s = sb.ToString();
+            Console.WriteLine(s);
+
+        }
+        
+    }
+    
+}
