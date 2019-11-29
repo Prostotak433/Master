f593f95cfbcac76194c7e1e4e08841420c58f596
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-29 22:00:53 +0200

    4. Class Exception

---
+++ Class Exception/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Class_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int[] numbers = new int[4];
+                numbers[7] = 9;     // IndexOutOfRangeException
+
+                int x = 5;
+                int y = x / 0;  // DivideByZeroException
+                Console.WriteLine($"Результат: {y}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Возникло исключение DivideByZeroException");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
