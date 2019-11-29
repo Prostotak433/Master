101e2c7574d758de6aac7e098f54e280b3f992e1
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-29 21:32:29 +0200

    4. Try Catch Finally

---
+++ Try catch finally/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Try_catch_finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число");
+            int x;
+            string input = Console.ReadLine();
+            if (Int32.TryParse(input, out x))
+            {
+                x *= x;
+                Console.WriteLine("Квадрат числа: " + x);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+            Console.Read();
+
+        }
+    }
+}
