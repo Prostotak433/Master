00fe60e44891ebff539c32cbffdd537d26bc00ee
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-29 21:50:04 +0200

    4. Block Catch  exception filters

---
+++ Exception filters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exception_filters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 5;
+
+            try
+            {
+
+                int result = x / y;
+                Console.WriteLine($"Результат: {y}");
+            }
+            catch(Exception ex) when (x ==0 )
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+    }
+}
