e0857d687b1ea2bffe7854768d2e3dade2902f7b
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-30 20:00:11 +0200

    4.  Exception generation and operator trow

---
+++ Exception generation and operator trow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exception_generation_and_operator_trow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    Console.WriteLine("Введите строку:");
+                    string mes = Console.ReadLine();
+                    if(mes.Length>6)
+                    {
+                        throw new Exception("Ддина строки больше 6 символов");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Возникло исключение");
+                    throw;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
