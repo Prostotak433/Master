639eb8aa00924cb45893597d133e05062010024a
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-12-03 14:07:25 +0200

    5. Аpplication Delegate

---
+++ Аpplication Delegate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Аpplication_Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Account
+    {
+        int _sum;
+        public Account(int sum)
+        {
+            _sum = sum;
+        }
+        public int CurrentSum
+        {
+            get { return _sum; }
+        }
+        public void Put(int sum)
+        {
+            _sum += sum;
+        }
+        public void Withdraw(int sum)
+        {
+            if(sum<=_sum)
+            {
+                _sum -= sum;
+            }
+        }
+        public delegate void AccountStateHangler(string message);
+        AccountStateHangler _del;
+        public void RegisterHandler(AccountStateHangler del)
+        {
+            _del = del;
+        }
+    }
+
+}
