7b3f12c51ba699602d2112ba76117bf7afec1262
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-12-04 15:57:57 +0200

    5. Anonim Method delegate

---
+++ Anonim Method/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Anonim_Method
+{
+    class Program
+    {
+        delegate void MessageHandler(string message);
+        delegate int Add(int x, int y );
+        static void Main(string[] args)
+        {
+            Add add = delegate (int x, int y)
+            {
+                return x + y;
+            };
+            Console.WriteLine(add(20, 30));
+            Console.WriteLine(add(10, 760));
+
+            MessageHandler handler = delegate 
+            {
+                Console.WriteLine("serj");
+            };
+            handler("Serj TOP");
+            Console.Read();
+        
+        }
+    }
+}
