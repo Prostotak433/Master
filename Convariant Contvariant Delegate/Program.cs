ac401a379e6813edca25d79daf5c78f1acd1f691
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-12-06 21:12:36 +0200

    Covariantnost Convariantnost Delegate

---
+++ Convariant Contvariant Delegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Convariant_Contvariant_Delegate
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+    class Client : Person { }
+    class Program
+    {
+        delegate Person PersonFactory(string name);
+        static void Main(string[] args)
+        {
+            PersonFactory personDel;
+            personDel = BuildClient;
+            Person p = personDel("Tom");
+
+        }
+        private static Client BuildClient(string name)
+        {
+            return new Client { Name = name };
+        }
+    }
+}
