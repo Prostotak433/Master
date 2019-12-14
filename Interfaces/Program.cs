7fd7bc19f07f5ac1a0685247c991287725fcd360
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-12-14 22:25:47 +0200

    6.  Interface

---
+++ Interfaces/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Interfaces
+{
+    interface Ispeed
+    {
+        const int minSpeed = 0;
+        static int maxSpeed = 70;
+        void Move();
+        string Name { get; set; }
+        delegate void Move(string mess);
+        event Move MoveEvent;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
