b770d5f4c6d04b5e5f43f573e21303f5c70e3725
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-12 16:37:20 +0200

    Abstract Class

--- Abstract class/Program.cs
+++ Abstract class/Program.cs
@@ -36,7 +36,7 @@ namespace Abstract_class
     }
     class Employee : Person
     {
-        public string Position { get; set; } // должнось
+        public string Position { get; set; } // должность
 
         public Employee(string name, string position)
             : base(name)
