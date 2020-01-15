e0aca3c363409103a3b9472c6cfc0d4bec70c912
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-15 15:55:38 +0200

    Abstract Class

--- Abstract class/Program.cs
+++ Abstract class/Program.cs
@@ -14,7 +14,7 @@ namespace Abstract_class
     }
     abstract class Person
     {
-      public string Name { }
+      public string Name { get; set; }
       public Person(string name)
         {
             Name = name;
