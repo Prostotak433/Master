8ac0aaf1317c454f1b1d8dea3fd5633ad4ed22c5
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-17 17:14:27 +0200

    Explicit implementation of interfaces

---
+++ Explicit implementation of interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Explicit_implementation_of_interfaces
+{
+    interface IAction
+    {
+        void Move();
+    }
+    class BaseAction : IAction
+    {
+        void IAction.Move()
+        {
+            Console.WriteLine("Move in Base class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseAction action = new BaseAction();
+            ((IAction)action).Move();
+            IAction action2 = new BaseAction();
+            action2.Move();
+        }
+    }
+}
