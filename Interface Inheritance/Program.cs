240ca75e25620f640df049efc344830566ea74e4
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-18 16:40:16 +0200

    6. Interface Inheritance

---
+++ Interface Inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Interface_Inheritance
+{
+    interface IAction
+    {
+        void Move();
+    }
+    interface IRunAction:IAction
+    {
+        void Run();
+    }
+    class BaseAction:IRunAction
+    {
+        public void Move()
+        {
+            Console.WriteLine("Move");
+        }
+        public void Run()
+        {
+            Console.WriteLine("Run");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
