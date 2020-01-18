14eb21e0b0ae313e32fb93944e75ded36f4380d4
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-18 16:21:45 +0200

    6. Interface implementation in base and derived classes

---
+++ Interface implementation in base and derived classes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Interface_implementation_in_base_and_derived_classes
+{
+    interface IAction
+    {
+        void Move();
+    }
+     class BaseAction : IAction
+    {
+        public  void Move()
+        {
+            Console.WriteLine("Base Action");
+        }
+    }
+    class HeroAction : BaseAction, IAction
+    {
+        public new void Move()
+        {
+            Console.WriteLine("Hero Action");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseAction action1 = new BaseAction();
+            action1.Move();
+            BaseAction action2 = new HeroAction();
+            action2.Move();
+            IAction action3 = new BaseAction();
+            action3.Move();
+            IAction action4 = new HeroAction();
+            action4.Move();
+        }
+    }
+}
