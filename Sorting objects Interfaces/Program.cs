527111e95344321af63fb93cc5b656bb55dd62dc
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-19 15:27:42 +0200

    6. Sorting objects. Interfaces

---
+++ Sorting objects Interfaces/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sorting_objects_Interfaces
+{
+    interface IComparer
+    {
+        int Compare(object o1, object o2);
+    }
+    class PeopleComparer : IComparer<Person>
+    {
+        public int Compare(Person p1, Person p2)
+        {
+            if (p1.Name.Length > p2.Name.Length)
+                return 1;
+            else if (p1.Name.Length < p2.Name.Length)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+          
+        }
+    }
+}
