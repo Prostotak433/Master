ca366855f1d1ecf1e5e5a354933b43eba862473b
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-19 15:42:07 +0200

    6.Covariance and contravariance of generic interfaces

---
+++ Covariance and contravariance of generic interfaces/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Covariance_and_contravariance_of_generic_interfaces
+{
+    interface IBank<out T>
+    {
+        T CreatAccount(int sum);
+    }
+    class Bank<T> : IBank<T> where T : Account, new()
+    {
+        public T CreateAccount(int sum)
+        {
+            T acc = new T();  // создаем счет
+            acc.DoTransfer(sum);
+            return acc;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
