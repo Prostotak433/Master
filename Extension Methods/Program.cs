cd9037458581272e1a98b168657c290849487efd
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-20 12:38:04 +0200

    7. Extension Methods

---
+++ Extension Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Extension_Methods
+{
+    public static class StringExec
+    {
+        public static int CharCount(this string str, char c)
+        {
+            int counter = 0;
+            for(int i = 0; i<str.Length; i++)
+            {
+                if (str[i] == c)
+                    counter++;
+            }
+            return counter;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Привет мир";
+            char c = 'и';
+            int i = s.CharCount(c);
+            Console.WriteLine(i);
+            Console.Read();
+        }
+    }
+}
