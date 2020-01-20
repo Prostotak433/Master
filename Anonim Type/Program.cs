2f15ea14b243d3fae37eafd65b2295e1c80b98d6
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-20 13:23:09 +0200

    7. Anonim Type

--- Anonim Type/Program.cs
+++ Anonim Type/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Anonim_Type
 {
+    class User
+    {
+        public string Name { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            User tom = new User();
+            int age = 34;
+            var student = new { tom.Name, age };
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.age);
+
+            var people[] = new[]
+            {
+                new{Name="bob"},
+                new{Name="tom"}
+            };
+            Console.Read();
         }
     }
 }
