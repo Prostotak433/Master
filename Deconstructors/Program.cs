c532bdf3011a30258db0f173ba3b238c9056ebd5
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-21 20:28:02 +0200

    7.Deconstructors

--- Deconstructors/Program.cs
+++ Deconstructors/Program.cs
@@ -2,11 +2,22 @@
 
 namespace Deconstructors
 {
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public void Deconstruct(out string name, out int age)
+        {
+            name = this.Name;
+            age = this.Age;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person = new Person { Age = 33, Name = "Tom" };
+            
         }
     }
 }
