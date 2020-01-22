ec50344460d2b047ef48e3093f79899ef01c7f18
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-22 17:19:33 +0200

    7. Local functions

--- Local functions/Program.cs
+++ Local functions/Program.cs
@@ -8,7 +8,7 @@ namespace Local_functions
         {
             var result = GetResult(new int[] { -3, -2, -1, 0, 1, 2, 3 });
             Console.WriteLine(result);
-            
+            Console.Read();
         }
         static int GetResult(int[] numbers)
         {
