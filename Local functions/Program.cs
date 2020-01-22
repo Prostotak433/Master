d9449aa454263b37b698c20e0f192c040ba57729
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-22 17:10:32 +0200

    7. Local functions

---
+++ Local functions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Local_functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = GetResult(new int[] { -3, -2, -1, 0, 1, 2, 3 });
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        static int GetResult(int[] numbers)
+        {
+            int limit = 0;
+            bool IsMoreThan(int number)
+            {
+                return number > limit;
+            }
+            int result = 0;
+            for(int i =0; i<numbers.Length; i++)
+            {
+                if(IsMoreThan(numbers[i]))
+                {
+                    result += numbers[i];
+                }
+            }
+            return result;
+        }
+    }
+}
