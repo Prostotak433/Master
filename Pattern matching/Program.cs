b1f40e2f6dd0fcd3a21409412c6b65551c82ee3e
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-23 16:30:09 +0200

    7. Pattern matching

---
+++ Pattern matching/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pattern_matching
+{
+    class Employee
+    {
+        public virtual void Work()
+        {
+            Console.WriteLine("I work");
+        }
+    }
+    class Manager:Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine("GO go GO Work");
+        }
+        public bool IsonVacation { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
