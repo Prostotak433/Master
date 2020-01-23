df213265b78e4c877132fddf16fcf3c3d7550c99
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-23 16:50:31 +0200

    7.Pattern Switcg

---
+++ Pattern Switcg/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pattern_Switcg
+{
+
+    class Person
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Language { get; set; }
+        static string GetMessage(Person p) => p switch
+        {
+            { Language: "english" } => "Hello!",
+            { Language: "german", Status: "admin" } => "Hallo!",
+            { Language: "french" } => "Salut!",
+            { } => "Hello world!"
+        };
+    }
+}
---
+++ Pattern Switcg/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pattern_Switcg
+{
+   
+    class Program
+    {
+        static int Select(int op, int a, int b)
+        {
+            switch (op)
+            {
+                case 1: return a + b;
+                case 2: return a - b;
+                case 3: return a * b;
+                default: throw new ArgumentException("Non cod operation");
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = Select(1, 4, 10);
+                Console.WriteLine(x);
+
+                x = Select(4, 4, 10);
+                Console.WriteLine(x);
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
