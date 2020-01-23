92c2a4188ca457e2b25794be1f8cc74e2b1cff1d
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-23 16:58:47 +0200

    7. Link Variables and Link Returns

---
+++ Link Variables and Link Return/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Link_Variables_and_Link_Return
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            ref int xRef = ref x;
+        }
+    }
+}
