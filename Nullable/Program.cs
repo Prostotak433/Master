18650474ee026e549a1fad21641aceb3c676d55d
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-23 17:08:08 +0200

    7. Nullable

---
+++ Nullable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nullable
+{
+    struct State
+    {
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = 7;
+            Console.WriteLine(x.Value);
+            Nullable<State> state = new State() { Name = "bob" };
+            Console.WriteLine(state.Value.Name);
+            Console.Read();
+        }
+    }
+}
