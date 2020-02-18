3d18a385a77f2dd4a32e32f83bcb41ce07e6d724
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-02-18 17:08:05 +0200

    Factory

---
+++ Factory/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory = new Factory();
+            Car toyota = factory.create("Toyota");
+            Car Audi = factory.create("audi");
+
+
+        }
+    }
+    interface Car
+    {
+        void drive();
+    }
+    class Toyota: Car
+    {
+        public void drive()
+        {
+            Console.Write("Drive a Toyota");
+        }
+    }
+    class Audi: Car
+    {
+        public void drive()
+        {
+            Console.Write("drive a Audi");
+        }
+    }
+    class Factory
+    {
+        public Car create(string typeOfCar)
+        {
+            switch (typeOfCar)
+            {
+                case "Toyota":return new Toyota();
+                case "Audi":return new Audi();
+                default: return null;
+            }
+        }
+
+    }
+}
