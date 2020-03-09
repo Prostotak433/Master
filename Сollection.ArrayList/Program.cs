2d3f49bdf647a4deba42d8e6572ab0b0caa5342d
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-09 16:29:04 +0200

    Collection.ArrayList

---
+++ Сollection.ArrayList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+namespace Сollection.ArrayList1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list1 = new ArrayList() { 4.4, "GI", 22, };
+            ArrayList list = new ArrayList();
+            list.Add(2.2);
+            list.Add(55);
+            list.AddRange(new string[] { "Hello", "World" });
+            
+            foreach(object o in list)
+            {
+                Console.Write(o);
+            }
+            list.RemoveAt(0);
+            list.Reverse();
+            Console.WriteLine(list[0]);
+            for(int i=0; i<list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.Read();
+        }
+    }
+}
