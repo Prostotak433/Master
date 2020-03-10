a4e595a402aaa3204f56660f7e13f81f3a521732
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-10 18:10:32 +0200

    Collection.Queue<T>

---
+++ Collection.LisT1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection.LisT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 13, 22, 4, 3, 101 };
+            numbers.Add(2);
+            numbers.AddRange(new int[] { 11, 33, 21 });
+            numbers.Insert(0, 666);
+            numbers.RemoveAt(1);
+            foreach(int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+            List<Person> people = new List<Person>(3);
+            people.Add(new Person() { Name = "Lexa" });
+            people.Add(new Person() { Name = "Vlad" });
+            foreach(Person p in people)
+            {
+                Console.WriteLine(p.Name);
+            }
+        }
+        class Person
+        {
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ Collections.Queue1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections.Queue1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+            numbers.Enqueue(3);
+            numbers.Enqueue(5);
+            numbers.Enqueue(8);
+            int queueElement = numbers.Dequeue();
+            int queueElement1 = numbers.Dequeue();
+            Console.WriteLine(queueElement);
+            Console.WriteLine(queueElement1);
+
+            Queue<Person> people = new Queue<Person>();
+            people.Enqueue(new Person() { Name = "Tom" });
+            people.Enqueue(new Person() { Name = "Bob" });
+            people.Enqueue(new Person() { Name = "Sem" });
+
+            Person pp = people.Peek();
+            Console.WriteLine(pp.Name);
+
+            Console.WriteLine("Сейчас в очереди {0} человек", people.Count);
+
+            foreach(Person p in people)
+            {
+                Console.WriteLine(p.Name);
+            }
+            Person person = people.Dequeue();
+            Console.WriteLine(person.Name);
+                
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
