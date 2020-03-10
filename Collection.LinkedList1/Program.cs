50d0de4f45f8e5194a29d04877213e0cdd9a0b44
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-10 17:43:43 +0200

    Collection.LinkedList<T>

---
+++ Collection.LinkedList1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection.LinkedList1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> numbers = new LinkedList<int>();
+            numbers.AddLast(1);
+            numbers.AddFirst(2);
+            numbers.AddAfter(numbers.Last, 3);
+            foreach(int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            LinkedList<Person> people = new LinkedList<Person>();
+            LinkedListNode<Person> tom = people.AddLast(new Person() { Name = "Tom" });
+            people.AddLast(new Person() { Name = "Bob" });
+            people.AddFirst(new Person() { Name = "Den" });
+
+            Console.WriteLine(tom.Previous.Value.Name);
+            Console.WriteLine(tom.Next.Value.Name);
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+}
