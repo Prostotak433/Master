3fa4604116efcca0b44f60b4a7c10c23be0ed98f
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-10 18:36:20 +0200

    Collections.Stack<T>

---
+++ Collection.Stack1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection.Stack1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<double> numbers = new Stack<double>();
+            numbers.Push(2.3);
+            numbers.Push(3.6);
+            numbers.Push(4.7);
+            double stackElement = numbers.Pop();
+            Console.WriteLine(stackElement);
+
+            Stack<Person> people = new Stack<Person>();
+            people.Push(new Person() { Name = "Tom", Age = 3 });
+            people.Push(new Person() { Name = "Bob", Age = 4 });
+            people.Push(new Person() { Name = "Sem", Age = 5 });
+
+            foreach(Person p in people)
+            {
+                Console.WriteLine(p.Name);
+                Console.WriteLine(p.Age);
+            }
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
