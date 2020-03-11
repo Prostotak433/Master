d260c973df6c03e20eaa1cc630d6e98dd55b266a
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-11 17:25:30 +0200

    Collecionts.OperatorYield

---
+++ Collecionts.OperatorYield/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+
+namespace Collecionts.OperatorYield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+            foreach(int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+            Console.Read();
+        }
+    }
+    class Book
+    {
+        public string Name { get; set; }
+        public Book(string name)
+        {
+            Name = name;
+        }
+    }
+    class Library
+    {
+        private Book[] books;
+        public Library()
+        {
+            books = new Book[] { new Book("Красное и чёрное"), new Book("Отцы и дети"), new Book("Война и Мир") };
+        }
+        public int Length
+        {
+            get { return books.Length; }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            for(int i = 0; i<books.Length; i ++)
+            {
+                yield return books[i];
+            }
+        }
+
+    }
+    class Numbers
+    {
+        public IEnumerator GetEnumerator()
+        {
+            for(int i = 0; i<6; i++)
+            {
+                yield return i * i;
+            }
+        }
+    }
+}
