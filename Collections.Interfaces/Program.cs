2bc8228fce0fe0ea09c03d5fd98d0a1c4354cc26
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-11 17:07:08 +0200

    Collections.Interfaces

---
+++ Collections.Interfaces/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace Collections.Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Week week = new Week();
+            foreach(var day in week)
+            {
+                Console.WriteLine(day);
+            }
+            Console.Read();
+        }
+    }
+    
+    class Week: IEnumerable
+    {
+        string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday",
+                         "Friday", "Saturday", "Sunday" };
+        public IEnumerator GetEnumerator()
+        {
+            return days.GetEnumerator();
+        }
+    }
+
+}
