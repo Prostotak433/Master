93dcd21a0b984bcf6e98eefbfbb75ba226af10f3
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-13 16:41:36 +0200

    10. System.String

---
+++ System.String1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace System.String1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "hello";
+            string s2 = null;
+            string s3 = new String('a', 6);
+            string s4 = new String(new char[] { 'w', 'o', 'r', 'l', 'd' });
+            Console.WriteLine(s1,s4);
+        }
+    }
+}
