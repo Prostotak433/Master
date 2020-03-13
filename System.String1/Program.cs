66b956da583504044c157c01083428753edc6dc1
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-13 18:16:52 +0200

    System,.String.Format

--- System.String1/Program.cs
+++ System.String1/Program.cs
@@ -6,39 +6,20 @@ namespace System.String1
     {
         static void Main(string[] args)
         {
-            string s1 = " hello";
-            string s2 = " world";
-            string s3 = String.Concat(s1, " Уебень");
-            Console.WriteLine(s3);
-            string[] values = new string[] { s1, s2, s3 };
-            string s4 = String.Join(s1, values);
-            Console.WriteLine(s4);
-            char ch = 'o';
-            int indexofChar = s1.IndexOf(ch);
-            int Lastindex = s1.LastIndexOf(ch);
-            Console.WriteLine(Lastindex);
-            Console.WriteLine(indexofChar);
-            string text = "И поэтому все так произошло";
+            Person person = new Person { Name = "Tom", Age = 23 };
+            Console.WriteLine("Имя: {0}  Возраст: {1}", person.Name, person.Age);
+            double number = 33.12;
+            Console.WriteLine(number.ToString("C2"));
+            Console.WriteLine(number.ToString("P1"));
+            Console.WriteLine(number.ToString("d1"));
+            Console.WriteLine(number.ToString("f4"));
 
-            string[] words = text.Split(new char[] { ' ' });
-
-            foreach (string s in words)
-            {
-                Console.WriteLine(s);
-            }
-            string text1 = "Хороший день";
-            string subString = "замечательный ";
-
-            text = text.Insert(8, subString);
-            Console.WriteLine(text1);
-
-            string s11 = "Ebyby";
-            int ind = s11.Length - 1;
-            s11 = s11.Remove(1,2);
-            Console.WriteLine(s11);
 
         }
-
-        
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
