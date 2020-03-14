8ab3235b5671f60a16dc209619f673652d5635e5
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-14 20:22:01 +0200

    Class.Math1

---
+++ Class.Math1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Class.Math1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = Math.Asin(1);
+            Console.WriteLine(result);
+            double result1 = Math.Ceiling(4.3564);
+            Console.WriteLine(result1);
+            double resylt2 = Math.Cos(30);
+            Console.WriteLine(resylt2);
+            double result2 = Math.Pow(4, 5);
+            Console.WriteLine(result2);
+
+        }
+    }
+}
