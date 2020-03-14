c7c75d35f8d48797a4f6c03d1db0eda9a3b78a91
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-14 19:53:40 +0200

    12. Type.Lazy<T>

---
+++ Type.Lazy1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Type.Lazy1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+            reader.ReadBook();
+            reader.ReadEbook();
+        }
+    }
+    class Reader
+    {
+        Lazy<Libary> libary = new Lazy<Libary>();
+        public void ReadBook()
+        {
+            libary.Value.GetBook();
+            Console.WriteLine("Читаем бумажную книгу");
+        }
+
+        public void ReadEbook()
+        {
+            Console.WriteLine("Читаем книгу на компьютере");
+        }
+    }
+    
+    class Libary
+    {
+        private string[] books = new string[99];
+        public void GetBook()
+        {
+            Console.WriteLine("Выдаем книгу читателю");
+        }
+    }
+}
