1f6f27b767048d31e4b62bf90a5f0d050fa13144
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-14 17:40:01 +0200

    Date.Time Format

--- DataTime1/Program.cs
+++ DataTime1/Program.cs
@@ -6,25 +6,24 @@ namespace DataTime1
     {
         static void Main(string[] args)
         {
-            DateTime date1 = new DateTime();
-            Console.WriteLine(date1);
-            Console.WriteLine(DateTime.MinValue);
-            Console.WriteLine(DateTime.MinValue);
-            DateTime date2 = new DateTime(2020, 3, 14);
-            Console.WriteLine(date2.ToLongDateString());
-            DateTime date3 = new DateTime(2020, 3, 14, 17, 11, 33);
-            Console.WriteLine(date3.DayOfWeek);
-
-            Console.WriteLine(DateTime.Now);
-            Console.WriteLine(DateTime.UtcNow);
-            Console.WriteLine(DateTime.Today);
-
-            DateTime date4 = new DateTime(1582, 10, 5);
-            Console.WriteLine(date4.DayOfWeek);
-
-            DateTime date5 = new DateTime(2020, 3, 14, 17, 19, 33);
-            DateTime date6 = new DateTime(2020, 3, 15, 21, 19, 33);
-            Console.WriteLine(date5.Subtract(date6));
+            DateTime now = DateTime.Now;
+            Console.WriteLine("D: " + now.ToString("D"));
+            Console.WriteLine("d: " + now.ToString("d"));
+            Console.WriteLine("F: " + now.ToString("F"));
+            Console.WriteLine("f: {0:f}", now);
+            Console.WriteLine("G: {0:G}", now);
+            Console.WriteLine("g: {0:g}", now);
+            Console.WriteLine("M: {0:M}", now);
+            Console.WriteLine("O: {0:O}", now);
+            Console.WriteLine("o: {0:o}", now);
+            Console.WriteLine("R: {0:R}", now);
+            Console.WriteLine("s: {0:s}", now);
+            Console.WriteLine("T: {0:T}", now);
+            Console.WriteLine("t: {0:t}", now);
+            Console.WriteLine("U: {0:U}", now);
+            Console.WriteLine("u: {0:u}", now);
+            Console.WriteLine("Y: {0:Y}", now);
+            
         }
     }
 }
