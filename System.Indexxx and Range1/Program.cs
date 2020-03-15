f009aa5d1242d4a9e58de71451105072adbee616
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-15 15:23:34 +0200

    System.Index and System.Range

---
+++ System.Indexxx and Range1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace System.Indexxx_and_Range1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Index myIndex1 = 2;     // третий элемент
+            Index myIndex2 = ^2;    // предпоследний элемент
+
+            string[] people = { "Tom", "Bob", "Sam", "Kate", "Alice" };
+            string selected1 = people[myIndex1];    // Sam
+            string selected2 = people[myIndex2];    // Kate
+            Console.WriteLine(selected1);
+            Console.WriteLine(selected2);
+
+            string[] people = { "Tom", "Bob", "Sam", "Kate", "Alice" };
+            string[] peopleRange = people[1..4]; // получаем 2, 3 и 4-й элементы из массива
+            foreach (var person in peopleRange)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
