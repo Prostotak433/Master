902f161d34a6ca2b84ec1f186860423e15988a36
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-15 14:02:16 +0200

    Method.Parse and TryParse

---
+++ Method.ParseAndTryParse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Method.ParseAndTryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse("30,30");
+            Console.WriteLine(a);
+
+            IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+            double b = double.Parse("23.56", formatter);
+
+            int number;
+            Console.WriteLine("Введите строку");
+            string imput = Console.ReadLine();
+
+            bool result = int.TryParse(imput, out number);
+            if (result == true)
+                Console.WriteLine("Успешно");
+            else
+                Console.WriteLine("Не успешно");
+
+        }
+    }
+}
