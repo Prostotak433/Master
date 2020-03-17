946a23f91a0db88a96f70597620a2fbce4b2675d
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-17 17:02:29 +0200

    13. Class.Thread

---
+++ Class.Thread/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Class.Thread1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = Thread.CurrentThread;
+            Console.WriteLine($"Имя потока: {t.Name}");
+            t.Name = "Метод Main";
+            Console.WriteLine($"Имя потока: {t.Name}");
+            Console.WriteLine($"Запущен ли поток: {t.IsAlive}");
+            Console.WriteLine($"Приоритет потока: {t.Priority}");
+            Console.WriteLine($"Статус потока: {t.ThreadState}");
+
+            Console.WriteLine($"Домен приложения: {Thread.GetDomain().FriendlyName}");
+            Console.ReadLine();
+        }
+    }
+}
