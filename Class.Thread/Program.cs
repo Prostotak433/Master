11a7fc12633e8b44e45f5d8283259d87801763b1
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-19 15:41:24 +0200

    13. Class.Thread. Semaphore

--- Class.Thread/Program.cs
+++ Class.Thread/Program.cs
@@ -5,30 +5,39 @@ namespace Class.Thread1
 {
     class Program
     {
-        static Mutex mutexObj = new Mutex();
-        static int x = 0;
         static void Main(string[] args)
         {
-            for (int i = 0; i < 5; i++)
+            for(int i = 1; i < 6; i++)
             {
-                Thread myThread = new Thread(Count);
-                myThread.Name = $"Поток {i}";
-                myThread.Start();
+                Reader reader = new Reader(i);
             }
             Console.ReadLine();
-        }       
-        public static void Count()
+        }
+    }    
+    class Reader
+    {
+        static Semaphore sen = new Semaphore(3, 3);
+        Thread myThread;
+        int count = 3;
+        public Reader(int i)
         {
-            mutexObj.WaitOne();
-            x = 1;
-            for (int i = 1; i < 9; i++)
+            myThread = new Thread(Read);
+            myThread.Name = $"Читатель {i.ToString()}";
+            myThread.Start();
+        }
+        public void Read()
+        {
+            while(count > 0)
             {
-                Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
-                x++;
-                Thread.Sleep(100);
+                sen.WaitOne();
+                Console.WriteLine($"{Thread.CurrentThread.Name} входит в библиотеку");
+                Console.WriteLine($"{Thread.CurrentThread.Name} читает");
+                Thread.Sleep(1000);
+                Console.WriteLine($"{Thread.CurrentThread.Name} покидает библиотеку");
+                sen.Release();
+                count--;
+                Thread.Sleep(1000);
             }
-            mutexObj.ReleaseMutex();
-          
         }
     }
 }
