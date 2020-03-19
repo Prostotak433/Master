5604a138546f183fcfbb5473ce308b6970dd6790
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-19 15:28:39 +0200

    13. Class.Thread.Mutex

--- Class.Thread/Program.cs
+++ Class.Thread/Program.cs
@@ -5,29 +5,29 @@ namespace Class.Thread1
 {
     class Program
     {
-        static AutoResetEvent waitHandler = new AutoResetEvent(true);
+        static Mutex mutexObj = new Mutex();
         static int x = 0;
         static void Main(string[] args)
         {
             for (int i = 0; i < 5; i++)
             {
                 Thread myThread = new Thread(Count);
-                myThread.Name = $"Поток {i.ToString()}";
+                myThread.Name = $"Поток {i}";
                 myThread.Start();
             }
             Console.ReadLine();
         }       
         public static void Count()
         {
-            waitHandler.WaitOne();
-                x = 1;
-                for (int i = 1; i < 9; i++)
-                {
-                    Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, x);
-                    x++;
-                    Thread.Sleep(100);
-                }
-            waitHandler.Set();
+            mutexObj.WaitOne();
+            x = 1;
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+                x++;
+                Thread.Sleep(100);
+            }
+            mutexObj.ReleaseMutex();
           
         }
     }
