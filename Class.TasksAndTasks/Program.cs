5c2cfd597b8633935eb05eb4c5de6e13d4a082c3
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-26 17:11:56 +0200

    14.2 Class.Task.Task

--- Class.TasksAndTasks/Program.cs
+++ Class.TasksAndTasks/Program.cs
@@ -8,35 +8,34 @@ namespace Class.Tasks1111
     {
         static void Main(string[] args)
         {
-            var outer = Task.Factory.StartNew(() =>
-            {
-                Console.WriteLine("Внешняя задача стартонула....");
-                var inner = Task.Factory.StartNew(() =>
-                {
-                    Console.WriteLine("Внутрення задача стартонула...");
-                    Thread.Sleep(2000);
-                    Console.WriteLine("Внутрення задача завершилась ...");
-                }, TaskCreationOptions.AttachedToParent);
-            });
-            outer.Wait();
-            Console.WriteLine("Завершение Main");
-            
+            Task<int> task1 = new Task<int>(() => Factorial(5));
+            task1.Start();
+            Console.WriteLine($"Факториал числа 5 равен {task1.Result}");
 
-            Task[] tasks1 = new Task[3]
+            Task<Book> task2 = new Task<Book>(() =>
             {
-                new Task(() => Console.WriteLine("First Task")),
-                new Task(() => Console.WriteLine("Second Task")),
-                new Task(() => Console.WriteLine("Third Task"))
-            };
-            foreach (var i in tasks1)
-                i.Start();
-            Task[] tasks2 = new Task[3];
-            int j = 1;
-            for (int i = 0; i < tasks2.Length; i++)
-                tasks2[i] = Task.Factory.StartNew(() => Console.WriteLine($"Task{j++}"));
-            Console.WriteLine("Завершение метода Маин");
+                return new Book { Title = "Война и мир", Author = "Л. Толстой" };
+            });
+            task2.Start();
+            Book b = task2.Result;  
+            Console.WriteLine($"Название книги: {b.Title}, автор: {b.Author}");
+
             Console.ReadLine();
-            
         }
+        static int Factorial(int x)
+        {
+            int result = 1;
+            for (int i = 1; i < x; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+    
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
     }
 }
