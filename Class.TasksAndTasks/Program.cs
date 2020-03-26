b320cf7f71a2d0c86ffc5b0082205fb235cca545
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-26 16:57:05 +0200

    14.2. Class.WorkWitchClassTask

--- Class.TasksAndTasks/Program.cs
+++ Class.TasksAndTasks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Class.Tasks1111
@@ -7,18 +8,35 @@ namespace Class.Tasks1111
     {
         static void Main(string[] args)
         {
-            Task task1 = new Task(Display);
-            task1.Start();
-            task1.Wait();
+            var outer = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Внешняя задача стартонула....");
+                var inner = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Внутрення задача стартонула...");
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Внутрення задача завершилась ...");
+                }, TaskCreationOptions.AttachedToParent);
+            });
+            outer.Wait();
+            Console.WriteLine("Завершение Main");
+            
+
+            Task[] tasks1 = new Task[3]
+            {
+                new Task(() => Console.WriteLine("First Task")),
+                new Task(() => Console.WriteLine("Second Task")),
+                new Task(() => Console.WriteLine("Third Task"))
+            };
+            foreach (var i in tasks1)
+                i.Start();
+            Task[] tasks2 = new Task[3];
+            int j = 1;
+            for (int i = 0; i < tasks2.Length; i++)
+                tasks2[i] = Task.Factory.StartNew(() => Console.WriteLine($"Task{j++}"));
             Console.WriteLine("Завершение метода Маин");
             Console.ReadLine();
-           
-        }
-        static void Display()
-        {
-            Console.WriteLine("Начало работы метода Display");
-
-            Console.WriteLine("Завершение работы метода Display");
+            
         }
     }
 }
