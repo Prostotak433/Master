ea07b082a3ea5245f42aa8f31e886d98f7cfb2bb
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-03-26 16:20:07 +0200

    14.1 System.Threading.Tasks.

---
+++ Class.TasksAndTasks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Class.Tasks1111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = new Task(Display);
+            task1.Start();
+            task1.Wait();
+            Console.WriteLine("Завершение метода Маин");
+            Console.ReadLine();
+           
+        }
+        static void Display()
+        {
+            Console.WriteLine("Начало работы метода Display");
+
+            Console.WriteLine("Завершение работы метода Display");
+        }
+    }
+}
