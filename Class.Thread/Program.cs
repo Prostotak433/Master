caa43ea197bc955349583f38a4b48794c1862508
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-06-22 15:27:31 +0300

    Book.Pattert(this),Method

--- Class.Thread/Program.cs
+++ Class.Thread/Program.cs
@@ -6,18 +6,31 @@ namespace Class.Thread1
     {
         static void Main(string[] args)
         {
-            
+            Motorcycle c = new Motorcycle(5);
+            c.driverName = "Tiny";
+            Console.WriteLine("Rider name is {0}", c.driverName);
+            Console.ReadLine();
         }
     }        
     class Motorcycle
     {
         public int driverIntesity;
         public string driverName;
-        public Motorcycle() { }
-        public Motorcycle(int intesity) : this(intesity, " ") { }
-        public Motorcycle(string name) : this(0, name) { }
+        public Motorcycle()
+        {
+            Console.WriteLine("In default ctor");
+        }
+        public Motorcycle(int intesity) : this(intesity, " ")
+        {
+            Console.WriteLine("In ctor taking an int");
+        }
+        public Motorcycle(string name) : this(0, name)
+        {
+            Console.WriteLine("In ctor taking a string");
+        }
         public Motorcycle(int intesity, string name)
         {
+            Console.WriteLine("In master ctor");
             if(intesity > 10)
             {
                 intesity = 10;
