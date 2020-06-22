359016f70cac97b64f90c01cfcb9ccee7420ed9d
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-06-22 16:38:10 +0300

    Book.Static

--- Class.Thread/Program.cs
+++ Class.Thread/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Class.Thread;
+using System;
 
 namespace Class.Thread1
 {
@@ -6,38 +7,12 @@ namespace Class.Thread1
     {
         static void Main(string[] args)
         {
-            Motorcycle c = new Motorcycle(5);
-            c.driverName = "Tiny";
-            Console.WriteLine("Rider name is {0}", c.driverName);
+            SavingAccount s1 = new SavingAccount(50);
+            SavingAccount s2 = new SavingAccount(100);
+            Console.WriteLine("Interest Rate is : {0}", SavingAccount.GetInterestRate());
+            SavingAccount s3 = new SavingAccount(1000.50);
+            Console.WriteLine("Interest Rate is : {0}", SavingAccount.GetInterestRate());
             Console.ReadLine();
         }
-    }        
-    class Motorcycle
-    {
-        public int driverIntesity;
-        public string driverName;
-        public Motorcycle()
-        {
-            Console.WriteLine("In default ctor");
-        }
-        public Motorcycle(int intesity) : this(intesity, " ")
-        {
-            Console.WriteLine("In ctor taking an int");
-        }
-        public Motorcycle(string name) : this(0, name)
-        {
-            Console.WriteLine("In ctor taking a string");
-        }
-        public Motorcycle(int intesity, string name)
-        {
-            Console.WriteLine("In master ctor");
-            if(intesity > 10)
-            {
-                intesity = 10;
-            }
-            driverIntesity = intesity;
-            driverName = name;
-        }
-
     }
 }
---
+++ Class.Thread/SavingAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class.Thread
+{
+    public class SavingAccount
+    {
+        public double currBalance;
+        public static double currInterestRate;
+        public SavingAccount(double balance)
+        {
+            currInterestRate = 0.04;
+            currBalance = balance;
+        }
+        public static void SetIneterestRate(double newRate)
+        {
+            currInterestRate = newRate;
+        }
+        public static double GetInterestRate()
+        {
+            return currInterestRate;
+        }
+    }
+}
