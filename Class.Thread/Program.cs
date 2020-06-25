99728014068fcb7b0938326f614a3d302d9609bf
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-06-25 19:14:58 +0300

    Book.OOP

--- Class.Thread/Program.cs
+++ Class.Thread/Program.cs
@@ -1,18 +1,35 @@
-﻿using Class.Thread;
-using System;
+﻿using System;
 
-namespace Class.Thread1
+namespace Class.Thread
 {
     class Program
     {
         static void Main(string[] args)
         {
-            SavingAccount s1 = new SavingAccount(50);
-            SavingAccount s2 = new SavingAccount(100);
-            Console.WriteLine("Interest Rate is : {0}", SavingAccount.GetInterestRate());
-            SavingAccount s3 = new SavingAccount(1000.50);
-            Console.WriteLine("Interest Rate is : {0}", SavingAccount.GetInterestRate());
-            Console.ReadLine();
+            Car car = new Car();
+            car.TurnOnRadio(false);
+            car.numberPage = 30_000_000;
+            car.Name = "Serj";
+            Console.WriteLine(car.Name);
+        }
+    }
+    class Radio
+    {
+        public void Power(bool turnOn)
+        {
+            Console.WriteLine("Radio on {0}", turnOn);
+        }
+    }
+    class Car
+    {
+        
+        private string _name;
+        private Radio myRadio = new Radio();
+        public int numberPage;
+        public string Name { get { return _name; } set { _name = value; } }
+        public void TurnOnRadio(bool onOff)
+        {
+            myRadio.Power(onOff);
         }
     }
 }
