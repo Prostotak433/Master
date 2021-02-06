bd7b7763ab53b262f189fcd027fbe7d9d5f3850b
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2021-02-06 15:51:52 +0200

    remember

---
+++ Remember/Remember/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Remember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+            int result = 0;
+            foreach(int i in numbers)
+            {
+                if (i > 0)
+                    result++;
+            }
+            Console.WriteLine(result);
+
+        }
+        
+    }
+}
